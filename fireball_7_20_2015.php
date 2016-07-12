f082afe6e97d4429efde474343bf351e3ae34040
timsdeepsky <timsdeepsky@gmail.com>
2016-07-11 19:18:25 -0500

    Add files via upload

---
+++ fireball_7_20_2015.php
@@ -0,0 +1,165 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+     
+
+
+
+ <head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+ 
+ <title>FIREBALL 7-20-2015</title> 
+
+   
+ <style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+
+.wrapper {
+	margin:0 auto;
+	max-width: 860px;
+	text-align: center;
+       
+        height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+       #footer {clear: both;
+		background: #ffffff;
+                font-weight: 900;
+		color: Black;
+		padding: 0px 0px 0px 0px;
+                margin: 0px 0px 0px 0px;
+		text-align: center;
+                border: 1px solid green;
+                width: auto;
+                height: auto;
+ 
+                }
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;padding: 0px 10px 0px 10px;
+}
+
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+  #playerMubAgxZtdIlb
+                {
+               
+		background: #000000;
+                font-weight: 900;
+		color: Black;
+		padding: 0px 0px 0px 0px;
+                margin-left: auto ;
+                margin-right: auto ;
+		text-align: center;
+                border: 1px solid green;
+                width: auto;
+                height: auto;
+                }
+
+.fireball_video
+{
+border:1px solid red;
+}
+
+
+
+
+
+
+
+
+
+
+
+ </style>
+
+
+
+
+ </head>                      
+             
+
+<body>
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+
+<h1> timcline.org</h1>
+  <h2>Fireball 7-20-2015....</h2>
+<p>
+The big Iowa fireball i captured on 7-20-2015....This was at 23:51pm(my local time)....
+This was captured by my Fireball meteor cams....
+</p>
+
+<p>
+This was a good event spotted over Iowa and Minnesota....It appears over my location in Southern Iowa and goes North towards Des Moines and over Minnesota....
+</p>
+
+
+
+
+<img src="https://timcline.org/images_timcline_org/meteors/fireball_7_20_2015_finalresult_done.jpg" alt="meteor" width="640" height="480" />
+<br />
+<br />
+
+
+
+
+  <p>Video of this event is below....Watch the upper right corner of the video....</p>
+
+ <video class="fireball_video" width="640" height="480" controls>
+  <source src="meteor_video_2015/fireball_7_20_2015_cut.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video> 
+
+
+
+
+<ul>
+<li><a href="https://www.timcline.org/meteor_video_2015/fireball_7_20_2015_cut.mp4">Iowa Fireball 7-20-2015</a></li>
+</ul>
+
+
+
+<br />
+
+
+
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+ </div>  
+                                                                                                  
+</body>
+
+
+                                                                                                          
+</body>
+</html>
