1a9d509ef70235cd8ea5b42a6354facfda83159b
timsdeepsky <timsdeepsky@gmail.com>
2016-07-11 19:27:06 -0500

    Add files via upload

---
+++ fireball_1_28_2014.php
@@ -0,0 +1,152 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+     
+
+
+
+ <head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+ 
+ <title>FIREBALL 1-28-2014</title> 
+
+   
+ <style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+
+.wrapper {
+	margin:0 auto;
+	max-width: 860px;
+	text-align: center;
+       
+        height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+       #footer {clear: both;
+		background: #ffffff;
+                font-weight: 900;
+		color: Black;
+		padding: 0px 0px 0px 0px;
+                margin: 0px 0px 0px 0px;
+		text-align: center;
+                border: 1px solid green;
+                width: auto;
+                height: auto;
+ 
+                }
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;padding: 0px 10px 0px 10px;
+}
+
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+  #playerMubAgxZtdIlb
+                {
+               
+		background: #000000;
+                font-weight: 900;
+		color: Black;
+		padding: 0px 0px 0px 0px;
+                margin-left: auto ;
+                margin-right: auto ;
+		text-align: center;
+                border: 1px solid green;
+                width: auto;
+                height: auto;
+                }
+
+ </style>
+
+ </head>                      
+             
+
+<body>
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+
+<h1> timcline.org</h1>
+  <h2>Fireball 1-28-2014....</h2>
+<p>
+The big Iowa fireball i captured on 1-28-2014....This was at 12:22am(my local time)....
+This was captured by my 3 meteor cams....
+</p>
+
+<p>
+This was a large event over my area in southern Iowa....
+</p>
+
+
+<img src="https://timcline.org/images_timcline_org/meteors/fireball_1_28_2014_tim_cline_color_direction.jpg" alt="meteor" width="640" height="480" />
+<br />
+<br />
+<img src="https://timcline.org/images_timcline_org/meteors/fireball_1_28_2014_tim_cline_south_cam_direction.jpg" alt="meteor" width="640" height="480" />
+<br />
+<br />
+<img src="https://timcline.org/images_timcline_org/meteors/Tim_Cline_fireball_20140128_002210.jpg" alt="meteor" width="640" height="480" />
+<br />
+<br />
+
+
+
+
+
+
+
+
+
+
+  <h2>Video from my cams are below here........</h2>
+
+<video class="fireball_video" width="640" height="480" controls>
+  <source src="meteor_video_2014/fireball_1_28_2014_tim_cline_color_cam_faststart.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video>
+
+<video class="fireball_video" width="640" height="480" controls>
+  <source src="meteor_video_2014/fireball_1_28_2014_x264_code_faststart.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</video>
+
+
+
+<br />
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+ </div>  
+                                                                                                  
+</body>
+
+
+                                                                                                          
+</body>
+</html>
