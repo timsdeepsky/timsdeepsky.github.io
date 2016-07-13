94daff8196036a5e49700f42c08324a1302b2648
timsdeepsky <timsdeepsky@gmail.com>
2016-07-12 19:58:54 -0500

    Add files via upload

---
+++ comets.php
@@ -0,0 +1,135 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+
+<title>Comets</title> 
+<style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+.wrapper 
+{
+margin:0 auto;
+max-width: 800px;
+text-align: center;
+height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+ 
+#footer 
+{
+clear: both;
+background: #ffffff;
+font-weight: 900;
+color: Black;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+text-align: center;
+border: 1px solid green;
+width: auto;
+height: auto;
+}
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;}
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+
+
+
+</style>
+
+</head>                      
+             
+<body>
+
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+<br />
+<h1> timcline.org</h1>
+<h2>Comets</h2>
+<p>Comet images i have taken from my Observatory....</p>
+
+
+<img src="https://timcline.org/Comet_images/comet_lovejoy_1_15_2015.jpg" alt="Comet" width="500" height="386" />
+<p>Comet Lovejoy(Jan-15-2015)</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/C_2009_P1_Comet_Garradd_tim_cline_sept_4_2011_lighter_web.jpg" alt="Comet" width="500" height="386" />
+<p>C-2009 P1 Comet Garradd(sept-4-2011)</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/C_2009_P1_Comet_Garradd_tim_cline_sept_4_2011_web.jpg" alt="Comet" width="500" height="386" />
+<p>C-2009 P1 Comet Garradd(sept-4-2011)</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet01-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet 177-p Barnard</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet02-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet03-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet04-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet05-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet06-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet07-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet</p>
+<br />
+<br />
+<img src="https://timcline.org/Comet_images/comet_17p_holmes_for_web-1.jpg" alt="Comet" width="387" height="291" />
+<p>Comet 17-p Holmes</p>
+<br />
+<br />
+
+
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+
+</div>                                                                                                    
+</body>
+</html>
