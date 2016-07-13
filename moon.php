ab635f54bf37469446a66689b7aaf64ff2bc855e
timsdeepsky <timsdeepsky@gmail.com>
2016-07-12 20:06:23 -0500

    Add files via upload

---
+++ moon.php
@@ -0,0 +1,214 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+
+<title>Moon</title> 
+<style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+.wrapper 
+{
+margin:0 auto;
+max-width: 800px;
+text-align: center;
+height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+ 
+#footer 
+{
+clear: both;
+background: #ffffff;
+font-weight: 900;
+color: Black;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+text-align: center;
+border: 1px solid green;
+width: auto;
+height: auto;
+}
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;}
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+
+
+
+</style>
+
+</head>                      
+             
+<body>
+
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+<br />
+<h1> timcline.org</h1>
+<h2>Moon</h2>
+<p>Moon images i have taken from my Observatory....</p>
+
+
+<img src="https://timcline.org/Moon_images/moon_10_13_2013_1.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon_10_13_2013_2.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon_10_13_2013_3.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon_10_13_2013_4.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon01.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon02.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon03.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon04.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon05.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon06.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon07.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon08.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon09.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon10.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon11.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon12.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon13.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon14.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon15.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon16.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon17.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon18.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon19.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon20.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon21.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon22.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon23.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon24.jpg" alt="Moon" width="400" height="300" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon_12-25-2007.jpg" alt="Moon" width="400" height="250" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/moon_tranquility_12-25-2007.jpg" alt="Moon" width="400" height="250" />
+<p>Moon</p>
+<br />
+<br />
+<img src="https://timcline.org/Moon_images/red_moon_tim_cline.jpg" alt="Moon" width="480" height="360" />
+<p>Moon</p>
+<br />
+<br />
+
+
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+</div>                                                                                                    
+</body>
+</html>
