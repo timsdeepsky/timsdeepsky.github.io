15094ea21734113f21730b72978b8fd1d354af93
timsdeepsky <timsdeepsky@gmail.com>
2016-07-12 19:16:04 -0500

    Add files via upload

---
+++ galaxies.php
@@ -0,0 +1,340 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+
+<title>Galaxies</title> 
+<style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+.wrapper 
+{
+margin:0 auto;
+max-width: 800px;
+text-align: center;
+height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+ 
+#footer 
+{
+clear: both;
+background: #ffffff;
+font-weight: 900;
+color: Black;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+text-align: center;
+border: 1px solid green;
+width: auto;
+height: auto;
+}
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;}
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+
+
+
+</style>
+
+</head>                      
+             
+<body>
+
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+<br />
+<h1> timcline.org</h1>
+<h2>Galaxies</h2>
+<p>Galaxy images i have taken from my Observatory....</p>
+<h2>Andromeda Galaxy is on the<br /> "Andromeda Galaxy" page....</h2>
+
+
+
+<img src="https://timcline.org/Galaxy_images/ngc891_10_12_2013.jpg" alt="Galaxy" width="400" height="300" />
+<p>Spiral Galaxy NGC-891</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc891_10_12_2013_1.jpg" alt="Galaxy" width="400" height="300" />
+<p>Spiral Galaxy NGC-891</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/caldwell12_6_29_2008.jpg" alt="Galaxy" width="400" height="400" />
+<p>Spiral Galaxy Caldwell 12</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/four-in-leo.jpg" alt="Galaxy" width="400" height="250" />
+<p>Four Galaxies in Leo</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-101-4-may-3-2008.jpg" alt="Galaxy" width="561" height="433" />
+<p>M-101 Pinwheel Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-101-tim-cline-big.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-101 Pinwheel Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/M-104.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-104 Sombrero Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/M_104_Sombrero_5_11_2007.jpg" alt="Galaxy" width="400" height="270" />
+<p>M-104 Sombrero Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-106-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-106 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-106-jan-26-2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-106 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-106.jpg" alt="Galaxy" width="382" height="250" />
+<p>M-106 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-108_3_3_march_3_08_fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-108 Barred Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-109-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-109 Barred Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-33_9-30-06.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-33 Triangulum Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_33_oct_3_2008.jpg" alt="Galaxy" width="500" height="386" />
+<p>M-33 Triangulum Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-51-10_stack-may-3-2008.jpg" alt="Galaxy" width="561" height="433" />
+<p>M-51 Whirlpool Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-51_3_3_march_3_08_fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-51 Whirlpool Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-51-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-51 Whirlpool Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-51_color_4_26_06.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-51 Whirlpool Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-63-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-63 Sunflower Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-63_6_6_march_3_08_fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-63 Sunflower Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_63_last_of_stack_of_twenty_one.jpg" alt="Galaxy" width="360" height="250" />
+<p>M-63 Sunflower Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/M-64_black_eye_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-64 Black Eye Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/M_64_color_4_26_06.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-64 Black Eye Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-65_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-65 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-66_and_m-65_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-65 and M-66 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-66_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-66 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-74-1-8-2008-40secstackof47-fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-74 Face on Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-77-dec-23-2006.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-77 Barred Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-81_1_2_2008_again.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-81 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-81-1-2-2008-tim-cline.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-81 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-81-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-81 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_81_feb_four_zero_six.jpg" alt="Galaxy" width="378" height="237" />
+<p>M-81 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_81_stack_of_161_tim_cline.jpg" alt="Galaxy" width="556" height="439" />
+<p>M-81 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-82_3_3_march_3_08_fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-82 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-82-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-82 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_82_lenticular_galaxy.jpg" alt="Galaxy" width="340" height="250" />
+<p>M-82 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m-87_6_6_march_3_2008_fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-87 Supergiant elliptical Galaxy(brightest radio source in the sky)</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_94_done.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-94 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_95_feb_four_zero_six.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-95 Barred Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-1957-tim-cline-dec-25_2006.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-1957 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_3115_spindle.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-3115 Spindle Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-3521-4-22-2007.jpg" alt="Galaxy" width="402" height="250" />
+<p>NGC-3521 Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_3628_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-3628 Edge on Spiral Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/NGC-3718-and-3729.jpg" alt="Galaxy" width="402" height="250" />
+<p>NGC-3718 and NGC-3729 Spiral Galaxies in Leo</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/NGC-3718-and-UGC-6527-Seyfert-galaxy.jpg" alt="Galaxy" width="402" height="250" />
+<p>NGC-3718 and smaller Galaxies in Leo</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/NGC-3718-anomoly.jpg" alt="Galaxy" width="402" height="250" />
+<p>NGC-3718 and NGC-3729 and something else went through the image while i was taking it</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-4217-and-satellite-galaxy-4-7-2007fullsize.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-4217 and satellite Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_4546_14minutes_of_exp_done_but_not_cut.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-4565 edge on Galaxy in Coma Berenices</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_4546_and_ngc_4494_14minutes_of_exp_done_but_not_cut.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-4565 edge on Galaxy in Coma Berenices and companion</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_463_and_satellite.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-4631 Galaxy and satellite</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-4725_6_22_2008.jpg" alt="Galaxy" width="561" height="433" />
+<p>NGC-4725 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-4725.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-4725 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_5033_and_Cor_Caroli_the_Alpha_star.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-5033 Galaxy and Cor Caroli Alpha star</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-5907.jpg" alt="Galaxy" width="389" height="250" />
+<p>NGC-5907 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-7331.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-7331 Galaxy with other Galaxies in the background</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc_7479.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-7479 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-7814-dec29-2007-for-website-1.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-7814 Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-891_oct_23_2006.jpg" alt="Galaxy" width="400" height="250" />
+<p>NGC-891 edge on Spiral Galaxy in Andromeda</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/ngc-891_oct_3_2008.jpg" alt="Galaxy" width="500" height="386" />
+<p>NGC-891 edge on Spiral Galaxy in Andromeda</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/sunflower_galaxy_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-63 Sunflower Galaxy</p>
+<br />
+<br />
+<img src="https://timcline.org/Galaxy_images/m_101_may_11_2007.jpg" alt="Galaxy" width="400" height="250" />
+<p>M-101 Pinwheel Galaxy</p>
+<br />
+<br />
+
+
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+</div>                                                                                                    
+</body>
+</html>
