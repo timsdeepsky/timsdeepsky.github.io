c0f8d9e4ea5bff8b53a49eff572870b47aa66e31
timsdeepsky <timsdeepsky@gmail.com>
2016-07-12 19:39:22 -0500

    Add files via upload

---
+++ nebula.php
@@ -0,0 +1,208 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+
+<title>Nebula</title> 
+<style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+.wrapper 
+{
+margin:0 auto;
+max-width: 800px;
+text-align: center;
+height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+ 
+#footer 
+{
+clear: both;
+background: #ffffff;
+font-weight: 900;
+color: Black;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+text-align: center;
+border: 1px solid green;
+width: auto;
+height: auto;
+}
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;}
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+
+
+
+</style>
+
+</head>                      
+             
+<body>
+
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+<br />
+<h1> timcline.org</h1>
+<h2>Nebula</h2>
+<p>Nebula images i have taken from my Observatory....</p>
+
+<img src="https://timcline.org/Nebula_images/orion_1_15_2015.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion-1-15-2015</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/lagoon_nebula_9_27_2008.jpg" alt="Nebula" width="500" height="386" />
+<p>Lagoon-Nebula-9-27-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m-16_6_29_2008.jpg" alt="Nebula" width="400" height="400" />
+<p>M-16-6-29-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m_16_jet_6_29_2008.jpg" alt="Nebula" width="400" height="400" />
+<p>M-16-jet-6-29-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m-27_6_22_2008.jpg" alt="Nebula" width="561" height="433" />
+<p> M-27-6-22-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m-42-1-2-2008-tim-cline.jpg" alt="Nebula" width="500" height="386" />
+<p>M-42-1-2-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m-42-1-8-2008-15seceachstackof141-fullsizebrighter.jpg" alt="Nebula" width="500" height="386" />
+<p>M-42-1-8-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m-42-1-8-2008-15seceachstackof141-fullsize.jpg" alt="Nebula" width="500" height="386" />
+<p>M-42-1-8-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m-57_6_22_2008.jpg" alt="Nebula" width="561" height="433" />
+<p>M-57-6-22-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/m_8_nebula_8_30_2008.jpg" alt="Nebula" width="500" height="386" />
+<p>M-8-Nebula-8-30-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula01.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion-6-22-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula02.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion region-6-22-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula03.jpg" alt="Nebula" width="500" height="386" />
+<p>M-16-Eagle Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula04.jpg" alt="Nebula" width="500" height="386" />
+<p>Ring Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula05.jpg" alt="Nebula" width="500" height="386" />
+<p>M-27-Dumbbell Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula06.jpg" alt="Nebula" width="500" height="386" />
+<p>Crab Nebula(supernova remnant)</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula07.jpg" alt="Nebula" width="500" height="386" />
+<p>Horse head Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula09.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion region</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula10.jpg" alt="Nebula" width="500" height="386" />
+<p>M-20-Trifid Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula11.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula12.jpg" alt="Nebula" width="500" height="386" />
+<p>Western Veil Nebula region</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula13.jpg" alt="Nebula" width="500" height="386" />
+<p>Flame Nebula-Orion region(and the star Alnitak)</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula14.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion region</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/nebula15.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion region</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/ngc_7635_bubble_nebula_10_10_2008.jpg" alt="Nebula" width="500" height="386" />
+<p>Bubble Nebula</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/omega_nebula_9_27_2008.jpg" alt="Nebula" width="500" height="386" />
+<p>Omega Nebula-9-27-2008</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/orion_nikon_12-25-2007.jpg" alt="Nebula" width="500" height="386" />
+<p>Orion Nebula wide field</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/rosette_region_1_2_2008.jpg" alt="Nebula" width="500" height="386" />
+<p>Rosette Nebula region</p>
+<br />
+<br />
+<img src="https://timcline.org/Nebula_images/rosette_region_fullsize_image_1_2_2008.jpg" alt="Nebula" width="748" height="577" />
+<p>Rosette Nebula region</p>
+<br />
+<br />
+
+
+
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+
+
+</div>                                                                                                    
+</body>
+</html>
