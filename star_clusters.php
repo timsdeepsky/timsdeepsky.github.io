8fc80764db6ceaf60857f6010366d3c94b9e57e9
timsdeepsky <timsdeepsky@gmail.com>
2016-07-14 18:07:48 -0500

    Add files via upload

---
+++ star_clusters.php
@@ -0,0 +1,183 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+
+<title>Star Clusters</title> 
+<style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+.wrapper 
+{
+margin:0 auto;
+max-width: 800px;
+text-align: center;
+height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+ 
+#footer 
+{
+clear: both;
+background: #ffffff;
+font-weight: 900;
+color: Black;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+text-align: center;
+border: 1px solid green;
+width: auto;
+height: auto;
+}
+
+img {
+   padding:2px;
+   border:2px solid #BA55D3;
+   background-color:#008080;
+}
+
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;}
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+
+
+
+</style>
+
+</head>                      
+             
+<body>
+
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+<br />
+<h1> timcline.org</h1>
+<h2>Star Clusters</h2>
+<p>Star Cluster images i have taken from my Observatory....</p>
+
+<img src="https://timcline.org/Starcluster_images/m15_star_cluster_tim_cline_9_2_2013.jpg" alt="Star Clusters" width="500" height="386" />
+<p>M-15 star cluster</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/arcturus_may_11_2007.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Arcturus</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/capella.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Capella</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/focus_star_one.jpg" alt="Star Clusters" width="500" height="386" />
+<p>My focus Star</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_13_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-13(hercules)</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m-13-22_stack-may-3-2008.jpg" alt="Star Clusters" width="561" height="433" />
+<p>Star Cluster M-13(hercules may-3-2008)</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_13_hercules_on_may_11_2007.jpg" alt="Star Clusters" width="485" height="359" />
+<p>Star Cluster M-13(hercules may-11-2007)</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_13_july_4_2008.jpg" alt="Star Clusters" width="519" height="400" />
+<p>Star Cluster M-13(hercules july-4-2008)</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_15_oct_3_2008.jpg" alt="Star Clusters" width="500" height="386" />
+<p>Star Cluster M-15(oct-3-2008)</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m-31-4-7-2007fullsize.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-31</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_3_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-3</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_3_2.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-3</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_37_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-37</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_3_july_4_2008_done.jpg" alt="Star Clusters" width="400" height="400" />
+<p>Star Cluster M-3</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_5_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-5</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_53_7_7_2008.jpg" alt="Star Clusters" width="325" height="325" />
+<p>Star Cluster M-53</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_5_7_7_2008.jpg" alt="Star Clusters" width="400" height="400" />
+<p>Star Cluster M-5</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/m_92_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Cluster M-92</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/pollux_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Pollux</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/star_cluster_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Open Cluster</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/star_orion_region.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star in Orion region</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/vega_1.jpg" alt="Star Clusters" width="486" height="366" />
+<p>Star Vega</p>
+<br />
+<br />
+<img src="https://timcline.org/Starcluster_images/vega_7_7_2008.jpg" alt="Star Clusters" width="400" height="400" />
+<p>Star Vega</p>
+<br />
+<br />
+
+
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+
+
+</div>                                                                                                    
+</body>
+</html>
