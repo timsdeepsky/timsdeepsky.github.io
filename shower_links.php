c69180e0bddce2575e0df7143cd968dcf6402967
timsdeepsky <timsdeepsky@gmail.com>
2016-07-27 18:58:12 -0500

    Add files via upload

---
+++ shower_links.php
@@ -0,0 +1,143 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en">
+
+     
+
+
+
+ <head>
+
+
+<link href='https://timcline.org/favicon.ico' rel='icon' type='image/x-icon'/>    
+ 
+ <title>Meteor showers</title> 
+
+   
+ <style type="text/css" media="screen">
+
+body
+{
+background-color:#000000;
+}
+
+
+
+.wrapper {
+	margin:0 auto;
+	max-width: 860px;
+	text-align: center;
+       height: 100%;
+border: 2px solid green;
+color: #000000;
+background-color: #000000;
+}
+       #footer {clear: both;
+		background: #ffffff;
+                font-weight: 900;
+		color: Black;
+		padding: 0px 0px 0px 0px;
+                margin: 0px 0px 0px 0px;
+		text-align: center;
+                border: 1px solid green;
+             
+ 
+                }
+h1 {color:#66CDAA;text-align:center;}
+h2 {color:#DAA520;text-align:center;}
+p {color:#DAA520;text-align:center;padding: 0px 10px 0px 10px;
+}
+
+
+a:link {color:#FF0000;}      /* unvisited link */
+a:visited {color:#00FF00;}  /* visited link */
+a:hover {color:#FF00FF;}  /* mouse over link */
+a:active {color:#0000FF;}  /* selected link */
+
+ul#navigation_links_showers a		  
+{
+color: #00FFFF;  /*this is color of text in grid*/
+text-decoration: none;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+display: block;
+
+} 
+
+ul#navigation_links_showers a:hover
+{
+background: red;
+color: #00FFFF;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 0px;
+
+}
+
+ul#navigation_links_showers
+{
+font-style: italic;
+margin-left: 0; 
+padding-left: 0;
+color: #00FFFF;
+padding: 0px 0px 0px 0px;
+margin: 0px 0px 0px 20px;
+
+}
+
+
+
+
+ </style>
+
+
+
+
+ </head>                      
+             
+
+<body>
+
+<div class="wrapper">
+
+<?php include('link_include.php'); ?>
+
+<h1> timcline.org</h1>
+  <h2>Meteor showers....</h2>
+<p>
+Meteor showers my cameras have caught....
+</p>
+
+
+<ul id="navigation_links_showers">
+<li><a href="https://www.timcline.org/south_taurids_2015.php">My South Taurid meteor shower results 2015</a></li>
+<li><a href="https://www.timcline.org/perseids_2015.php">My Perseid meteor shower results 2015</a></li>
+<li><a href="https://www.timcline.org/perseids_2014.php">My Perseid meteor shower results 2014</a></li>
+<li><a href="https://www.timcline.org/geminids_2013.php">My Geminids meteor shower results 2013</a></li>
+<li><a href="https://www.timcline.org/orionids_2013.php">My Orionids meteor shower results 2013</a></li>
+<li><a href="https://www.timcline.org/draconids_2013.php">My Draconids meteor shower results 2013</a></li>
+<li><a href="https://www.timcline.org/perseids_2013.php">My Perseid meteor shower results 2013</a></li>
+<li><a href="https://www.timcline.org/delta_aquarids_2013.php">My Delta Aquarids meteor shower results 2013</a></li>
+<li><a href="https://www.timcline.org/geminids_2012.php">My Geminids meteor shower results 2012</a></li>
+<li><a href="https://www.timcline.org/leonids_2012.php">My Leonids meteor shower results 2012</a></li>
+<li><a href="https://www.timcline.org/north_south_taurids_2012.php">My North and South Taurids meteor shower results 2012</a></li>
+<li><a href="https://www.timcline.org/orionids_2012.php">My Orionids meteor shower results 2012</a></li>
+<li><a href="https://www.timcline.org/delta_aquarid_and_perseids_2012.php">My Delta Aquarid and Perseid meteor shower results 2012</a></li>
+<li><a href="https://www.timcline.org/meteors/meteor_showers.php">Meteor Showers pre 2012</a></li>
+</ul>
+
+<br />
+
+<div id="footer">
+<?php include('copyright.php'); ?>
+</div>
+
+ </div>  
+                                                                                                  
+</body>
+
+                                                                                                        
+</body>
+</html>
